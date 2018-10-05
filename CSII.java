f7c0b4b3842a100a65edd8d76eb01fad19ed99b3
marcosrodrigueza <marcooseslavalight@gmail.com>
2018-10-05 20:59:28 +0200

    Basic Grav Move included

--- CSII.java
+++ CSII.java
@@ -15,10 +15,10 @@ public class CSII extends AdvancedRobot
 	public void run()
 	{
 		// Initialization of the robot--------------------------------------------------
-		setColors(Color.DARK_GRAY,Color.cyan,Color.cyan); // body,gun,radar
+		setColors(Color.DARK_GRAY,Color.cyan,Color.yellow); // body,gun,radar
 		
-		final double battleWidth = getBattleFieldWidth();
-		final double battleHeight = getBattleFieldHeight();
+		/*final double battleWidth = getBattleFieldWidth();
+		final double battleHeight = getBattleFieldHeight();*/
 		
 		addCustomEvent( new RadarTurnCompleteCondition(this));
 		setAdjustRadarForGunTurn(true);
@@ -29,6 +29,7 @@ public class CSII extends AdvancedRobot
 		while(true)
 		{
 			// Replace the next 4 lines with any behavior you would like
+			antiGravMove();
 			execute();
 		}
 	}
@@ -60,7 +61,7 @@ public class CSII extends AdvancedRobot
 			{
 				if((enemyList.elementAt(i)).equals(enemy))
 				{
-						enemyList.get(i).printVals(); //DEBUG
+						
 						enemyList.get(i).updateEnemy(enemy); //It could be useful to add time actualization margin 
 						//to reduce computational loads
 						enemyList.get(i).printVals(); //DEBUG
@@ -126,5 +127,102 @@ public class CSII extends AdvancedRobot
 			radarTurn = maxBearing + Math.signum(maxBearing)*22.5; //22.5 Correction factor
 		setTurnRadarRight(radarTurn);
 		radarDirection =(int) Math.signum(radarTurn);
+	}
+	
+	void antiGravMove()
+	{
+   		double xforce = 0;
+	    double yforce = 0;
+	    double force;
+	    double ang;
+	    GravPoint p;
+	    
+	    //cycle through all the enemies.  If they are alive, they are repulsive.  Calculate the force on us
+		for(int i = 0; i < enemyList.size(); i++)
+		{
+				p = enemyList.get(i).getGravPoint();
+		        force = p.power/Math.pow(getRange(getX(),getY(),p.x,p.y),2);
+		        //Find the bearing from the point to us
+		        ang = Utils.normalRelativeAngle(Math.PI/2 - Math.atan2(getY() - p.y, getX() - p.x)); 
+		        //Add the components of this force to the total force in their respective directions
+		        xforce += Math.sin(ang) * force;
+		        yforce += Math.cos(ang) * force;
+			
+	    } //Could be convenient to add middel points for mele functions
+	    
+	    /**The following four lines add wall avoidance.  They will only affect us if the bot is close 
+	    to the walls due to the force from the walls decreasing at a power 3.**/
+	    xforce += 5000/Math.pow(getRange(getX(), getY(), getBattleFieldWidth(), getY()), 3);
+	    xforce -= 5000/Math.pow(getRange(getX(), getY(), 0, getY()), 3);
+	    yforce += 5000/Math.pow(getRange(getX(), getY(), getX(), getBattleFieldHeight()), 3);
+	    yforce -= 5000/Math.pow(getRange(getX(), getY(), getX(), 0), 3);
+	    
+	    //Move in the direction of our resolved force.
+	    goTo(getX()-xforce,getY()-yforce);
+	}
+	
+	/**Move towards an x and y coordinate**/
+	void goTo(double x, double y)
+	{
+	    double dist = 20; 
+	    double angle = Math.toDegrees(absBearing(getX(),getY(),x,y));
+	    double r = turnTo(angle);
+	    setAhead(dist * r);
+	}
+
+
+	/**Turns the shortest angle possible to come to a heading, then returns the direction the
+	the bot needs to move in.**/
+	
+	int turnTo(double angle)
+	{
+	    double ang;
+    	int dir;
+	    ang = Utils.normalRelativeAngle(getHeading() - angle);
+	    if (ang > 90) {
+	        ang -= 180;
+	        dir = -1;
+	    }
+	    else if (ang < -90) {
+	        ang += 180;
+	        dir = -1;
+	    }
+	    else {
+	        dir = 1;
+	    }
+	    setTurnLeft(ang);
+	    return dir;
+	}
+	
+	public double absBearing( double x1,double y1, double x2,double y2 )
+	{
+		double xo = x2-x1;
+		double yo = y2-y1;
+		double h = getRange( x1,y1, x2,y2 );
+		if( xo > 0 && yo > 0 )
+		{
+			return Math.asin( xo / h );
+		}
+		if( xo > 0 && yo < 0 )
+		{
+			return Math.PI - Math.asin( xo / h );
+		}
+		if( xo < 0 && yo < 0 )
+		{
+			return Math.PI + Math.asin( -xo / h );
+		}
+		if( xo < 0 && yo > 0 )
+		{
+			return 2.0*Math.PI - Math.asin( -xo / h );
+		}
+		return 0;
+	}
+	
+	public double getRange( double x1,double y1, double x2,double y2 )
+	{
+		double xo = x2-x1;
+		double yo = y2-y1;
+		double h = Math.sqrt( xo*xo + yo*yo );
+		return h;	
 	}
 }
